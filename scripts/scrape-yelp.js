149ad68cae6b673c13ce94e6fcac494277a9adb2
sopheak <sopheakneak@live.com>
2017-11-19 22:32:48 -0800

    resolving promises issue

--- scripts/scrape-yelp.js
+++ scripts/scrape-yelp.js
@@ -2,50 +2,74 @@
 
 const fs = require('file-system');
 const yelp = require('yelp-fusion');
+var parse = require('csv-parse');
+
 const clientId = '7NxVBjQ49tgV_HKdQbuPNw';
 const clientSecret = 'bIrmTdzhVMFgS2PrLBtFMptgMyhHGiRslX6j3a5wOCV9RTynvJMrmfKuKiHtBMrC';
 var yelpObject = [];
 
 // api info to get census tract
 const axios = require('axios');
-//const headUrl = "https://geocoding.geo.census.gov/geocoder/geographies/onelineaddress?address=";
-//const tailUrl = "&benchmark=9&vintage=910&format=json";
 const headUrl = 'http://data.fcc.gov/api/block/find?format=json&';
-// https://geocoding.geo.census.gov/geocoder/geographies/address?street=214%20W%20Main%20St&city=Ritzville&state=WA&zip=99169&benchmark=9&vintage=910&format=json
 
 
-// http://data.fcc.gov/api/block/find?format=json&latitude=47.1273723&longitude=-118.3988154
-
-
-const searchRequest = {
-  latitude: 47.1508088,
-  longitude: -118.3988154,
-  categories: 'Restaurants',
-  radius: 4000,
-  limit: 50
-};
+// const searchRequest = {
+//   latitude: 47.1508088,
+//   longitude: -118.3988154,
+//   categories: 'Restaurants',
+//   radius: 4000,
+//   limit: 50
+// };
 var totalRestaurant = 0;
 
-function performYelpRequest() {
+performCall();
+
+function performCall() {
+  fs.readFile('./Seattle Census Tract Lat Long - Sheet1.csv', function (err, data) {
+    parse(data, { columns: true }, function (err, dataValue) {
+      performYelpRequest(dataValue);
+    })
+  })
+}
+
+function performYelpRequest(seattleCensus) {
   yelp.accessToken(clientId, clientSecret).then(response => {
     var client = yelp.client(response.jsonBody.access_token);
-    client.search(searchRequest).then(response => {
-      var result = response.jsonBody.businesses;
-      loopBusinessObjects(result);
-    });
+    for (var i = 0; i < seattleCensus.length; i++) {
+      var searchFields = {
+      latitude: seattleCensus[i].Latitude,
+      longitude: seattleCensus[i].Longitude,
+      categories: 'Restaurants',
+      radius: 4000,
+      limit: 50
+    }
+    runRequest(searchFields,client);
+  }
+    // client.search(searchRequest).then(response => {
+    //   var result = response.jsonBody.businesses;
+    //   loopBusinessObjects(result);
+    // });
   }).catch(e => {
     console.log(e);
   });
 }
 
+function runRequest(searchRequest,client) {
+  //debugger;
+  client.search(searchRequest).then(response => {
+    var result = response.jsonBody.businesses;
+    loopBusinessObjects(result);
+  });
+}
+
 function loopBusinessObjects(resultObject) {
   var index = 0;
-  totalRestaurant+=resultObject.length;
-  debugger;
+  totalRestaurant += resultObject.length;
+  //debugger;
   for (index; index < resultObject.length; index++) {
     var currentBusiness = resultObject[index];
     var census = new Promise(function (resolve, reject) {
-      var apiUrl = headUrl + '&latitude='+ currentBusiness.coordinates.latitude+'&longitude='+currentBusiness.coordinates.longitude;
+      var apiUrl = headUrl + '&latitude=' + currentBusiness.coordinates.latitude + '&longitude=' + currentBusiness.coordinates.longitude;
       axios.get(apiUrl)
         .then(function (response) {
           var code = response.data.Block.FIPS;
@@ -56,17 +80,17 @@ function loopBusinessObjects(resultObject) {
         });
     })
     census.then(function (fullfilled) {
-      createBusinessObject(currentBusiness,fullfilled);
+      createBusinessObject(currentBusiness, fullfilled);
     }).catch(function (error) {
       console.log(error.message);
     });
   }
 }
 
-function createBusinessObject(eachRestaurant,fips) {
-  var fipsSubstr = fips.toString().substring(5,10);
+function createBusinessObject(eachRestaurant, fips) {
+  var fipsSubstr = fips.toString().substring(5, 10);
   var census = parseInt(fipsSubstr);
-  debugger;
+  //debugger;
   var businessObject = {
     id: eachRestaurant.id,
     url: eachRestaurant.url,
@@ -85,8 +109,6 @@ function createBusinessObject(eachRestaurant,fips) {
   yelpObject.push(businessObject);
   if (totalRestaurant === yelpObject.length) {
     debugger;
-    fs.writeFile('scripts/testyelp.json', JSON.stringify(yelpObject, null, 2));
+    fs.writeFile('scripts/SeattleRestaurantsByCensusTracts.json', JSON.stringify(yelpObject, null, 2));
   }
 }
-
-performYelpRequest();
